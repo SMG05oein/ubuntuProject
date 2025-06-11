042594294bf20d83de150cf44f3ce684f676f563
dain0415 <hoa0117@naver.com>
2025-06-11 16:46:18 +0900

    map, map_naver 수정

--- map_naver.php
+++ map_naver.php
@@ -11,7 +11,7 @@ include_once "./topMenu.php";
 		 <div>
       <h1><img src="./img/map_naver.png" width = '70'> 네이버 지도 사용법</h1>
     <p><strong>1단계:</strong> 네이버 지도에 들어갑니다. (<a href="https://map.naver.com" target="_blank">https://map.naver.com</a>)</p>
-    <p><strong>2단계:</strong> 화면 왼쪽 위에 있는 검색창에 가고 싶은 장소의 이름을 입력합니다.<br>예: 서울역, OO아파트, OO병원</p> <img src = "./img/map.png">
+    <p><strong>2단계:</strong> 화면 왼쪽 위에 있는 검색창에 가고 싶은 장소의 이름을 입력합니다.<br>예: 서울역, OO아파트, OO병원</p> <img src = "./img/map.png" alt = '네이버지도 검색창 이미지'>
     <p><strong>3단계:</strong> 나오는 목록에서 원하는 장소를 누릅니다.</p>
     <p><strong>4단계:</strong> '길찾기' 버튼을 눌러 어떻게 가는지 확인합니다.</p>
     <p><strong>TIP:</strong> 오른쪽 위에 있는 버튼으로 '버스', '지하철', '도보' 이동 방법도 확인할 수 있어요.</p>
